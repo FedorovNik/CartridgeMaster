e997495663a912d78afb761ae5cc4371c049bf31
Никита Федоров <fedorov_n@nizhbel.internal>
2026-03-11 15:22:22 +0300

    fix sqlite req bugs, custom logger, 1 db connection in lifespan

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, status
-from fastapi.responses import HTMLResponse, PlainTextResponse, RedirectResponse
+from fastapi import FastAPI, status, Request
+from fastapi.responses import HTMLResponse, PlainTextResponse, RedirectResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 
-import logging
 import sys
-import uvicorn
+import logging
+
+import uvicorn, copy
 from uvicorn.logging import DefaultFormatter
 from pydantic import BaseModel
 
@@ -21,29 +22,42 @@ import json
 import base64
 
 
-# Создаем базовый логгер
+AES_KEY = "My_Secret_Key_16"
+DB_NAME = "inventory.db"
+
+############################################## ЛОГГЕР #############################################################
+# 1. Создаем логгер
 logger = logging.getLogger("my_custom_logger")
 logger.setLevel(logging.INFO)
 
-# Используем DefaultFormatter как в uvicorn
-formatter = DefaultFormatter("%(levelprefix)s %(message)s")
+# формат лога: 
+# %(asctime)s - время
+# %(levelprefix)s - раскрашенный уровень (уже содержит цвета Uvicorn)
+# %(message)s - само сообщение
+fmt = "%(asctime)s | %(levelprefix)s %(message)s"
+date_fmt = "%Y-%m-%d %H:%M:%S"
 
-# Настраиваем вывод в консоль
+# Используем именно DefaultFormatter из uvicorn.logging
+# use_colors=True для сохранения цветов
+formatter = DefaultFormatter(fmt, datefmt=date_fmt, use_colors=True)
+
+# Настраиваем Handler
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(formatter)
+
+# Очищаем старые хендлеры, если они были (чтобы не дублировать логи)
+if logger.hasHandlers():
+    logger.handlers.clear()
+
 logger.addHandler(console_handler)
-
-
 
 hostname: str = socket.gethostname()
 local_ip: str = socket.gethostbyname(hostname)
 
-logger.info(f"IP-адрес: {local_ip}")
 logger.info(f"Имя хоста: {hostname}")
+logger.info(f"IP-адрес: {local_ip}")
 
-
-
-
+############################################## ЛОГГЕР #############################################################
 # Хранилище уникальных ID для каждого запроса от ТСД
 processed_requests = set()
 
@@ -58,54 +72,71 @@ async def clean_ids_task():
         processed_requests.clear()
         logger.info(f"Набор недавних ID-транзакций от ТСД очищен. Удалено: {total_cleared}")
 
+# Lifespan - механизм управления жизненным циклом приложения, 
+# который позволяет выполнять код перед началом работы приложения и после
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    ############ Установка соединения с базой ##########################
+    try:
+        app.state.db = await aiosqlite.connect(DB_NAME)
+        # WAL для конкурентного доступа, но мб это избыточно
+        await app.state.db.execute("PRAGMA journal_mode=WAL;")
+        logger.info("Соединение с БД установлено.")
+        # Закидываем в короткую переменную
+        db = app.state.db
+        
+    except Exception as e:
+        logger.error(f"Соединение с БД не установлено: {e}")
+        # Выбрасываем ошибку дальше. Uvicorn её поймает и остановит запуск сервера.
+        raise RuntimeError("Не удалось соединиться с базой данных!") from e
 
-    async with aiosqlite.connect(DB_NAME) as db:
-        # Картриджи
-        await db.execute("""
-            CREATE TABLE IF NOT EXISTS cartridges (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                cartridge_name TEXT NOT NULL,
-                quantity INTEGER DEFAULT 0,
-                last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-        """)
-        # Штрихкоды
-        await db.execute("""
-            CREATE TABLE IF NOT EXISTS barcodes (
-                barcode TEXT PRIMARY KEY,
-                cartridge_id INTEGER NOT NULL,
-                FOREIGN KEY (cartridge_id) REFERENCES cartridges(id)
-            )
-        """)
-        await db.execute("""
-            CREATE TABLE IF NOT EXISTS history (
-                increment INTEGER PRIMARY KEY AUTOINCREMENT,
-                cartridge_id INTEGER NOT NULL,
-                cartridge_name INTEGER NOT NULL,
-                delta INTEGER NOT NULL,
-                editor TEXT NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (cartridge_id) REFERENCES cartridges(id)
-            )
-        """)
-        logger.info(f"БД SQLite проинициализована или уже существует.")
-        await db.commit()
-
+    ############ Создание таблиц в базе если не существуют ############
+    # Картриджи
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS cartridges (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cartridge_name TEXT NOT NULL,
+            quantity INTEGER DEFAULT 0,
+            last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    # Штрихкоды
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS barcodes (
+            barcode TEXT PRIMARY KEY,
+            cartridge_id INTEGER NOT NULL,
+            FOREIGN KEY (cartridge_id) REFERENCES cartridges(id)
+        )
+    """)
+    # История изменений
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS history (
+            increment INTEGER PRIMARY KEY AUTOINCREMENT,
+            cartridge_id INTEGER NOT NULL,
+            cartridge_name TEXT NOT NULL,
+            delta INTEGER NOT NULL,
+            editor TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (cartridge_id) REFERENCES cartridges(id)
+        )
+    """)
+    logger.info(f"База данных проинициализована.")
+    await db.commit()
 
     # Запуск при старте функции очистки сета айдишников транзакций ТСД
     asyncio.create_task(clean_ids_task())
+
     yield
-    # При остановке можно добавить логику здесь
+
+    # Логика при остановке
+    await db.close()
+    logger.info(f"Соединение с БД закрыто.")
+
 
 app = FastAPI(lifespan=lifespan)
-
-
 # Монтажим папку с фронтом как /admin-ui
 app.mount("/admin-ui", StaticFiles(directory="frontend", html=True), name="admin-ui")
-AES_KEY = "My_Secret_Key_16"
-DB_NAME = "inventory.db"
+
 
 # В FastApi нужен класс для описания структуры входящих данных.
 # ТСД отправляет зашифрованный в base64 plain text, который после расшифровки представляет из себя json-структуру.
@@ -113,7 +144,7 @@ DB_NAME = "inventory.db"
 class ScanRequest(BaseModel):
     payload: str
 
-# Дешифратор для запросов от ТСД
+######################################### ШИФРАТОР И ДЕШИФРАТОР ДЛЯ ТСД ################################################
 def decrypt_payload(encrypted_b64: str):
     try:
         # Декодируем из Base64
@@ -121,73 +152,43 @@ def decrypt_payload(encrypted_b64: str):
         # Извлекаем IV (первые 16 байт) и зашифрованные данные
         iv = combined[:16]
         ciphertext = combined[16:]
-        
         # Настраиваем AES CBC
         cipher = AES.new(AES_KEY.encode('utf-8'), AES.MODE_CBC, iv)
-        
         # Расшифровываем и убираем Padding
         decrypted_bytes = unpad(cipher.decrypt(ciphertext), AES.block_size)
         return decrypted_bytes.decode('utf-8')
     except Exception as e:
         print(f"Ошибка расшифровки: {e}")
         return None
-
+    
 # Шифратор для ответа ТСД
 def encrypt_payload(data_str: str):
-    cipher = AES.new(AES_KEY.encode('utf-8'), AES.MODE_CBC) # IV сгенерируется сам
+    # IV сгенерируется сам
+    cipher = AES.new(AES_KEY.encode('utf-8'), AES.MODE_CBC) 
     ct_bytes = cipher.encrypt(pad(data_str.encode('utf-8'), AES.block_size))
     combined = cipher.iv + ct_bytes
     return base64.b64encode(combined).decode('utf-8')
 
-# Джокушка локера от любопытных глаз
-@app.get("/scan")
-async def trap_page():
-    # Мы используем CSS, чтобы растянуть картинку на весь экран
-    # и добавить красный фон для драматизма
-    # и вообще убрать нафиг в папку фронта
-    html_content = """
-    <html>
-        <head>
-            <title>Вы кто такие?</title>
-            <style>
-                body {
-                    margin: 0;
-                    padding: 0;
-                    background-color: #1a0000;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    overflow: hidden;
-                }
-                img {
-                    max-width: 90%;
-                    max-height: 90%;
-                    border: 10px solid black; 
-                    box-shadow: 0 0 50px rgba(255, 0, 0, 0.4);
-                }
-            </style>
-        </head>
-        <body>
-            <img src="admin-ui/static/403.png" alt="403 Forbidden - Вы кто такие?">
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
 
-#  Эндпоинт для ТСД 
+############################################# API для ТСД ##############################################################
 @app.post("/scan")
-# Указывает объект класса ScanRequest, который будет автоматически заполняться данными из тела запроса.
-async def process_scan(data: ScanRequest):
+# Объект data класса ScanRequest будет заполняться данными из тела запроса
+# C помощью Request получим состояние БД 
+async def process_scan(data: ScanRequest, request: Request):
+    # Получаем объект базы
+    db = request.app.state.db
+
     # Отправляем в дешифратор абракадабру, которая должна быть расшифрована в JSON-строки
     decrypted_json_str = decrypt_payload(data.payload)
-    
+
+    # Если функция дешифратор ничего не вернула..
     if not decrypted_json_str:
         return PlainTextResponse(
             encrypt_payload("Ошибка: AES-Ключ не совпадал на сервере или пакет поврежден!"),
             status_code=status.HTTP_400_BAD_REQUEST
         )
 
+    # Если расшифровалась, парсим полученный json
     try:
         inner_data = json.loads(decrypted_json_str)
         # ТСДшник формирует уникальный ID у каждого запроса
@@ -223,87 +224,99 @@ async def process_scan(data: ScanRequest):
         req_barcode = inner_data.get('barcode')
         # ТСД присылает 'action': 'add' или 'red'
         req_action = inner_data.get('action') 
-
         
-        async with aiosqlite.connect(DB_NAME) as db:
-            # Ищем штрихкод
-            cursor = await db.execute("SELECT cartridge_id FROM barcodes WHERE barcode = ?", (req_barcode,))
-            row = await cursor.fetchone()
+        # Ищем штрихкод
+        cursor = await db.execute("SELECT cartridge_id FROM barcodes WHERE barcode = ?", (req_barcode,))
+        row = await cursor.fetchone()
+        if not row:
+            # Устанавливаем код 404 (Not Found)
+            msg = f"Штрихкод {req_barcode} не привязан!"
+            return PlainTextResponse(encrypt_payload(msg), status_code=status.HTTP_404_NOT_FOUND)
+
+        # Берем id по этому штрихкоду
+        cartridge_id = row[0]
+
+        # Обновляем количество +1 в таблице cartridges
+        if req_action == 'add':
+            await db.execute("UPDATE cartridges SET quantity = quantity + 1 WHERE id = ?", (cartridge_id,))
+
+        # Обновляем количество -1 в таблице cartridges
+        else:
+            cursor = await db.execute("UPDATE cartridges SET quantity = quantity - 1 WHERE id = ? AND stock > 0", (cartridge_id,))
+            if cursor.rowcount == 0:
+                msg = "Ошибка: Остаток не может быть меньше нуля!"
+                return PlainTextResponse(encrypt_payload(msg), status_code=status.HTTP_409_CONFLICT)
             
-            if not row:
-                # Устанавливаем код 404 (Not Found)
-                msg = f"Штрихкод {req_barcode} не привязан!"
-                return PlainTextResponse(encrypt_payload(msg), status_code=status.HTTP_404_NOT_FOUND)
+        await db.commit()
 
-            # Нашли id по этому штрихкоду
-            cartridge_id = row[0]
-
-            # Обновляем количество +1 в таблице cartridges
-            if req_action == 'add':
-                await db.execute("UPDATE cartridges SET stock = stock + 1 WHERE id = ?", (cartridge_id,))
-
-            # Обновляем количество -1 в таблице cartridges
-            else:
-                cursor = await db.execute("UPDATE cartridges SET stock = stock - 1 WHERE id = ? AND stock > 0", (cartridge_id,))
-                if cursor.rowcount == 0:
-                    # rowcount - счетчик измененных строк
-                    
-                    msg = "Ошибка: Остаток не может быть меньше нуля!"
-                    return PlainTextResponse(encrypt_payload(msg), status_code=status.HTTP_409_CONFLICT)
-                
-            await db.commit()
-            # Получаем новый остаток для ответа
-            cursor = await db.execute("SELECT name, stock FROM cartridges WHERE id = ?", (cartridge_id,))
-            name, new_stock = await cursor.fetchone()
+        # Получаем новый остаток для ответа
+        cursor = await db.execute("SELECT cartridge_name, quantity FROM cartridges WHERE id = ?", (cartridge_id,))
+        name, new_stock = await cursor.fetchone()
 
         # Шифро-ответ ТСД: запрос обработан
         return PlainTextResponse(encrypt_payload(f"Имя: {name}\nШтрих-код:{req_barcode}\nОстаток: {new_stock}"))
 
-        
     except Exception as e:
         # Шифро-ответ ТСД: непонятный косяк на сервере
         return PlainTextResponse(encrypt_payload("Непредвиденная критическая ошибка сервера!"), status_code=500)
 
+################################## API для браузеров #############################################################
+# Джокушка локера от любопытных глаз
+@app.get("/scan")
+async def trap_page():
+    return FileResponse("frontend/scan.html")
 
-# ЧИСТЫЙ API ДЛЯ АДМИНКИ (JSON) 
+# Клиент при отправке get на сервак получает index.html вместе со скриптом app.js.
+# app.js выполняет get запрос к api-сервера /api/v1/cartridges 
 @app.get("/api/v1/cartridges")
-async def get_inventory():
-    async with aiosqlite.connect(DB_NAME) as db:
-        # Магия SQL: получаем имя, список штрихкодов через запятую и сумму остатков
-        cursor = await db.execute("""
-            SELECT 
-                c.id, 
-                c.cartridge_name,
-                c.quantity, 
-                GROUP_CONCAT(DISTINCT b.barcode) as barcodes
-            FROM cartridges c
-            LEFT JOIN barcodes b ON c.id = b.cartridge_id
-            GROUP BY c.id
-        """)
-        rows = await cursor.fetchall()
-        
-        return [
-            {
-                "id": r[0],
-                "name": r[1],
-                "stock": r[2],
-                "barcodes": r[3].split(",") if r[3] else []
-                
-            } for r in rows
-        ]
+async def get_inventory(request: Request):
+    db = request.app.state.db
+    # Магия SQL: получаем имя, список штрихкодов через запятую и сумму остатков
+    cursor = await db.execute("""
+        SELECT 
+            c.id, 
+            c.cartridge_name,
+            c.quantity, 
+            GROUP_CONCAT(DISTINCT b.barcode) as barcodes
+        FROM cartridges c
+        LEFT JOIN barcodes b ON c.id = b.cartridge_id
+        GROUP BY c.id
+    """)
+    rows = await cursor.fetchall()
+    # Возвращаем чистый json
+    return [
+        {
+            "id": r[0],
+            "name": r[1],
+            "stock": r[2],
+            "barcodes": r[3].split(",") if r[3] else []
+            
+        } for r in rows
+    ]
+    
+# Перенаправление пользователя на файл админки    
 @app.get("/")
 async def root_redirect():
-    # Перенаправляем пользователя сразу на файл админки
-    return RedirectResponse(url='/admin-ui/')
-@app.get("/admin-ui")
-async def root_redirect():
-    # Перенаправляем пользователя сразу на файл админки
     return RedirectResponse(url='/admin-ui/')
 
+# Перенаправление пользователя на файл админки
+@app.get("/admin-ui")
+async def root_redirect():
+    return RedirectResponse(url='/admin-ui/')
+
+
+################################## ЗАПУСК UVICORN #############################################################
 if __name__ == "__main__":
+    # Настраиваем формат: Дата Время | Уровень | Сообщение
+    log_config = copy.deepcopy(uvicorn.config.LOGGING_CONFIG)
+    # Меняем формат, добавляя %(asctime)s, НО оставляя настройки цвета
+    log_config["formatters"]["default"]["fmt"] = "%(asctime)s | %(levelprefix)s %(message)s"
+    log_config["formatters"]["access"]["fmt"] = "%(asctime)s | %(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s"
     
-    logger.info(f"Выполняется запуск uvicorn-процесса...")
+    # Указываем формат даты
+    log_config["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
+    log_config["formatters"]["access"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
     # Uvicorn слушает только запросы с сервера, на котором он запущен.
     # Сделано так, для того, чтобы нельзя было попаcть на него "в обход"
     # прокси caddy из локалки по http и 8080 порту
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080, log_config=log_config)
